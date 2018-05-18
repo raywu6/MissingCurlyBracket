c594b02347489a9aa95852a85ee4dd1cbc13fae1
Raymond Wu <rwu6@stuy.edu>
2018-05-18 05:53:30 -0400

    Update median var to bind to double value

--- rmed/RunMedDriver.java
+++ rmed/RunMedDriver.java
@@ -1,57 +1,55 @@
+// Team MissingCurlyBracket -- Raymond Wu, Woosuk Lee, Eric Wong
+// APCS2 pd8
+// HW50 -- Run Run Run, take a breath or two
+// 2018-05-17
+
 /**
    driver file for testing class RunMed
 
    (RunMed provides for calculating a running median
-    of a stream of input ints)
+   of a stream of input ints)
 
-    USAGE: Runnable as-is.
-    Uncomment RunMed-specific lines below once RunMed is implemented.
-    Compile, run fr same dir as RunMed.java
+   USAGE: Runnable as-is.
+   Uncomment RunMed-specific lines below once RunMed is implemented.
+   Compile, run fr same dir as RunMed.java
 
-    to feed numbers manually:
-    $ java RunMed
+   to feed numbers manually:
+   $ java RunMed
 
-    to feed in prepared set:
-    $ java RunMed < input.nums
-    (input.nums provided; must be in same dir)
-
-    -------------------------
-    Clyde "Thluffy" Sinclair
-    APCS2
-    2018-05-17
+   to feed in prepared set:
+   $ java RunMed < input.nums
+   (input.nums provided; must be in same dir)
 */
 
 import java.util.Scanner;
 
-public class RunMedDriver
-{
+public class RunMedDriver {
+    
+    public static void main( String[] args )
+    {
+	RunMed r = new RunMed();
 
-  public static void main( String[] args )
-  {
+	int n;
+	double median;
+	int count = 0;
+	Scanner sc = new Scanner( System.in );
 
-    // RunMed r = new RunMed();
+	while( sc.hasNextInt() ) {
+	    try {
+		n = sc.nextInt();
+		System.out.print("read " + n + "\n");
 
-    int n;
-    int median;
-    int count = 0;
-    Scanner sc = new Scanner( System.in );
+		count++;
+		System.out.print("this many ints have been seen: " + count + "\n");
 
-    while( sc.hasNextInt() ) {
-      try {
-        n = sc.nextInt();
-        System.out.print("read " + n + "\n");
+		r.add(n);
+		median = r.getMedian();
+		System.out.print("median is now " + median + "\n");
+	    } catch (Exception e) {
+		System.err.println("BOOP! probs w yer input:\n"+e);
+	    }
+	}
 
-        count++;
-        System.out.print("this many ints have been seen: " + count + "\n");
-
-        // r.add(n);
-        // median = r.getMedian();
-        // System.out.print("median is now " + median + "\n");
-      } catch (Exception e) {
-        System.err.println("BOOP! probs w yer input:\n"+e);
-      }
-    }
-
-  }//end main
+    }//end main
 
 }//end class
